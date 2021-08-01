2b06f9c663af6129670cde314c9506415d294810
kayoung yoon <rkdkdudjd@gmail.com>
2021-08-01 17:19:40 +0900

    test: add MemberRepositoryTest
    create tests for save(), findById(), findByName()

---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,61 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*; // assertThat을 그냥 사용하기 편하다!
+
+class MemoryMemberRepositoryTest { // 얘는 굳이 퍼블릭으로 하지 않아도 된다.
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member); // DB에 저장해따~~~
+
+        // 저장된 애를 찾아와볼가?
+        Member result = repository.findById(member.getId()).get(); // get으로 바로 꺼내는게 좋은 건 아닌데 테스트 코드니까 걍 해보자.
+        assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+}
