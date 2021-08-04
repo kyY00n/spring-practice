e9a569d0583fb04b2d97ea35cc2943746f4dc5d3
kayoung <rkdkdudjd@gmail.com>
2021-08-04 21:17:49 +0900

    feat: change database java memory to h2

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,12 +1,23 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource datasource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.datasource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -14,7 +25,8 @@ public class SpringConfig {
     }
 
     @Bean
-    public MemoryMemberRepository memoryMemberRepository() {
-        return new MemoryMemberRepository();
+    public MemberRepository memoryMemberRepository() {
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(datasource);
     }
 }
